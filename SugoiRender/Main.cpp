e7f824ec8f9ea9a6bf7f3f75c8ff933982e8a069
Geoffrey Tucker <gtuck123@yahoo.com>
2015-09-06 23:22:24 -0700

    Added SOIL image parsing and basic textures implementation

--- SugoiRender/Main.cpp
+++ SugoiRender/Main.cpp
@@ -1,107 +1,180 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL2/SOIL2.h>
 #include <iostream>
 #include "Shader.h"
 
+// Set up vertex data (and buffer(s)) and attribute pointers
 GLfloat vertices[] = {
-	// Positions         // Colors
-	0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Right
-	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // Bottom Left
-	0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // Top 
+    // Positions          // Colors           // Texture Coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left 
 };
 
-void key_callback(GLFWwindow* window, int key, int scanCode, int action, int mode) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, GL_TRUE);
-	}
-}
+GLuint indices[] = {
+    0, 1, 3, // First Triangle
+    1, 2, 3  // Second Triangle
+};
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scanCode, int action, int mode);
 
 GLuint generateDefaultVBO() {
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	// Copy our vertices array into a buffer for OpenGL to use
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	// Texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	return VBO;
 }
 
-/*GLuint generateDefaultEBO() {
+GLuint generateDefaultEBO() {
 	GLuint EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	return EBO;
-}*/
+}
+
+GLuint generateTexture(const char* imagePath) {
+	// First load the image into memory using SOIL
+	int width, height;
+	unsigned char* image = SOIL_load_image(imagePath, &width, &height, 0, SOIL_LOAD_RGB);
+	if(image == '\0') {
+		std::cout << "Unable to load image for texture: " << imagePath << std::endl;
+	}
+
+	// Bind our new OpenGL texture
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Set texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// Load image data into our OpenGL texture
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// Free image data from memory and unbind OpenGL texture
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return texture;
+}
 
 int main() {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "SugoiRender", nullptr, nullptr);
-	if (window == nullptr) {
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "SugoiRender", nullptr, nullptr);
+	if(window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
-	glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
 
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK) {
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if(glewInit() != GLEW_OK) {
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
 
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
+    // Build and compile our shader program
+    Shader shader("shader.vert", "shader.frag");
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
 
 	// Generate default vertex buffer object
-	glBindVertexArray(VAO);
+    glBindVertexArray(VAO);
 	GLuint VBO = generateDefaultVBO();
-	//GLuint EBO = generateDefaultEBO();
-	glBindVertexArray(0);
+	GLuint EBO = generateDefaultEBO();
+    glBindVertexArray(0);
 
-	// Generate shader program to use when we want to render an object
-	Shader shader("shader.vert", "shader.frag");
-	GLint vertexColorLocation = glGetUniformLocation(shader.Program, "ourColor");
+    // Load and create a texture 
+    GLuint texture1 = generateTexture("wood_container.jpg");
+	GLuint texture2 = generateTexture("awesome_face.png");
 
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    while (!glfwWindowShouldClose(window)) {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		GLfloat timeValue = glfwGetTime();
-		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-		
-		shader.Use();
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindVertexArray(0);
+        // Bind textures
+		glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+		glUniform1i(glGetUniformLocation(shader.Program, "Texture1"), 0);
 
-		glfwSwapBuffers(window);
+		glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+		glUniform1i(glGetUniformLocation(shader.Program, "Texture2"), 1);
+
+        // Activate shader
+        shader.Use();       
+        
+        // Draw container
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scanCode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
 	}
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-
-	glfwTerminate();
-	return 0;
 }
--- SugoiRender/shader.frag
+++ SugoiRender/shader.frag
@@ -1,9 +1,13 @@
 #version 330 core
 
-in vec3 ourColor;
+in vec3 Color;
+in vec2 TexCoord;
 
 out vec4 color;
 
+uniform sampler2D Texture1;
+uniform sampler2D Texture2;
+
 void main() {
-	color = vec4(ourColor, 1.0);
+	color = mix(texture(Texture1, TexCoord), texture(Texture2, TexCoord), 0.2);
 }
